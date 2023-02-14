bf9840b6c1801db51af75689758f1146de325c47
haaaein <haaaein@naver.com>
2023-02-14 17:14:39 +0900

    Change recommendation images

--- src/views/Recommend.vue
+++ src/views/Recommend.vue
@@ -10,7 +10,8 @@
       </div>
       <div class="border-bottom"></div>
       <h5 class="subtitle">
-        서로 다른 4개의 스타일을 가지고 있는 인스타그램 피드 예시들을 클릭하면서 새롭게 정렬되는 쇼핑몰 아이템들을 확인해보세요.
+        서로 다른 4개의 스타일을 가지고 있는 인스타그램 피드 예시들을 클릭하면서 새롭게 정렬되는
+        쇼핑몰 아이템들을 확인해보세요.
       </h5>
       <div class="border-bottom"></div>
     </div>
@@ -45,20 +46,20 @@ export default {
   },
   data() {
     return {
-      num: 0,
+      num: 2,
       item_list: [
         {
           influencer: "jenny",
           insta: "https://astlyi.s3.ap-northeast-2.amazonaws.com/influencer/jenny.png",
           item: [
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/aimons_301609432_6.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AVANDRESS_301586848_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/beyondcloset_301449969_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BINOVULAR_301663641_2.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ARCHAIC_301444389_2.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AMARDEW_301677576_1.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/aimons_301690736_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANGELO BIANCO_301484266_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/a.riff_301566545_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AMU_301662354_2.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/Arch The_301691759_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANGELO BIANCO_301484257_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/a.t.corner_301647436_6.jpg",
           ],
         },
         {
@@ -80,13 +81,13 @@ export default {
           insta: "https://astlyi.s3.ap-northeast-2.amazonaws.com/influencer/mino.png",
           item: [
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ADER ERROR_301403840_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AMONG_301416007_3.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/APIECEOFCAKE_301493909_1.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ADER ERROR_301404507_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ADER ERROR_301403757_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BENSIMON_301211954_2.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ACIDBLACK_300750223_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/alors_300518569_1.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BEGIN 2.0.2_301511954_2.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/APIECEOFCAKE_301447900_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANDTHEOTHER_301061946_2.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BEBUMS_301108118_7.jpg",
           ],
         },
         {
@@ -94,17 +95,13 @@ export default {
           insta: "https://astlyi.s3.ap-northeast-2.amazonaws.com/influencer/seojun.png",
           item: [
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANGELOBIANCO HOMME_301628580_3.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANGELOBIANCO HOMME_301628579_1.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/Another day of sun_301074107_1.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AVANDRESS_301541051_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BEMUSE MANSION_301626914_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/akro_300783306_5.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AUBOUR_300924579_3.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AFTERGLOW_301312729_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BALANT_300624534_1.jpg",
             "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ATHPLATFORM_301456110_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AVA MOLLI_300856103_4.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BAU by Bride And You_301277089_4.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/ANEDIT_301135263_2.jpg",
-            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/bibyseob_301207911_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/AUBOUR_301069619_3.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/alors_300848570_1.jpg",
+            "https://astlyi.s3.ap-northeast-2.amazonaws.com/wconcept/BEGIN 2.0.2_301477176_3.jpg",
           ],
         },
       ],
